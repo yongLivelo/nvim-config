a797014bce262cd44de18169722fd1bc456d4d17
yongLivelo <julioeduardolivelo@gmail.com>
2025-09-28 17:46:34 +0800

    Add git lazy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,4 +28,25 @@ return {
       },
     },
   },
+
+  -- nvim v0.8.0
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+  },
 }
