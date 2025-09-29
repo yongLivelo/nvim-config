dc896ac7978e1cc8052c975bf2422b1dceef0668
Julio Eduardo B. Livelo <94818606+yongLivelo@users.noreply.github.com>
2025-09-29 20:13:24 +0800

    Delete lua directory

--- lua/mappings.lua
+++
@@ -1,8 +0,0 @@
-require("nvchad.mappings")
--- add yours here
-local map = vim.keymap.set
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map("n", "<leader>lg", "<cmd>LazyGit<cr>")
-map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
